340617bbecf437b71490bc2154cd088cfc1524d9
gyro永不抽风 <1247006353@qq.com>
2022-04-11 13:10:11 +0800

    chore: nump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def make_email(prefix, domain):
 
 setuptools.setup(
 	name = 'baidutongji',
-	version = '0.1.1',
+	version = '0.1.2',
 	author = 'JeffersonQin',
 	author_email = make_email('1247006353', 'qq.com'),
 	packages = ['baidutongji'],
